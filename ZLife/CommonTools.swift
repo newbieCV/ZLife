36beeef4d7ab11b73cb427704a5fb52bcf777dfd
章肿鱼 <zhangzongyu00@gmail.com>
2022-03-05 19:30:37 +0800

    我的页 添加UICollectionView

--- ZLife/CommonTools.swift
+++ ZLife/CommonTools.swift
@@ -7,7 +7,20 @@
 
 import UIKit
 
+struct kScreen {
+    static let width = CommonTools.screenWidth
+    static let height = CommonTools.screenWidth
+}
+
 class CommonTools {
+    class func screenWidth() -> CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    class func screenHeight() -> CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
     // 裁剪图片到指定大小
     class func tailorImage(image: UIImage, newSize: CGRect) -> UIImage {
         UIGraphicsBeginImageContext(CGSize(width: newSize.width, height: newSize.height))
--- ZLife/MineViewController.swift
+++ ZLife/MineViewController.swift
@@ -6,10 +6,70 @@
 //
 
 import UIKit
+import SnapKit
 
 class MineViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configUI()
+    }
+    
+    func configUI() {
+        view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 6
+        layout.minimumInteritemSpacing = 8
+        let view = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        view.delegate = self
+        view.dataSource = self
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "a")
+        return view
+    }()
+}
+
+extension MineViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return 50
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell: UICollectionViewCell
+        if indexPath.section == 0 {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "a", for: indexPath)
+            cell.backgroundColor = .blue
+        } else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "a", for: indexPath)
+            cell.backgroundColor = .gray
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 0, left: 12, bottom: 10, right: 12)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 0 {
+            return CGSize(width: kScreen.width(), height: 200)
+        } else {
+            return CGSize(width: (kScreen.width() - 48) / 4, height: 60)
+        }
     }
 }
